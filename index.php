89b7f9ad414ed746b6ad16082bcc0f5b190f03c7
J. Gavin Ray <gavin@abmscale.com>
2013-02-13 08:35:29 -0800

    ABM Logo and index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+   <head>
+       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+       <title></title>
+   </head>
+   <body>
+       <p align="center"><img src="ABM.jpg"><br>
+       <p align="center">Today&rsquo;s date (according to this web server) is
+       <?php
+       echo date('l, F jS Y.');
+       ?><br>And the current time is: <?php
+       echo date('g:i:sa');
+       ?>.
+   </body>
+</html>
