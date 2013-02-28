fcd8b4d1d21079f660461e5604a6ba2e121de4d3
J. Gavin Ray <gavin@abmscale.com>
2013-02-28 07:01:31 -0800

    Added index.php & just to make sure Jon's commit doesn't done us

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
+<<<<<<< HEAD
+=======
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>
+            Testing 123
+        </title>
+    </head>
+        <body>
+            <form name="form1" method="POST" action="lab1.php"
+                <label>Number 1:</label><input type="text" name="firstoperand"><br>
+                <label>Number 2:</label><input type="text" name="secondoperand"><br>
+                <label>Operator:</label><input type="text" name="operator"><br>
+                <input type="submit" value="Calculate"/>
+            </form>
+        </body>
+</html>
+<!--
+>>>>>>> Just in case
 <html>
    <head>
        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-       <title>CSIS-78: ABM Scale Company - Product Tracking System</title>
+       <title>CSIS-78: Product Tracking System</title>
    </head>
    <body>
-       <p align="center"><img src="ABM.jpg"></p>
-       <h2><p align="center">ABM Scale Company - Product Tracking System</p></h2>
-       <h4><p align="center">by: J. Gavin Ray & Johnathan Adams</p></h4>
+       <h2><p align="center">Product Tracking System</p></h2>
+ 
        <p align="center">Today&rsquo;s date and time (according to this web server) is
        <?php
        echo date('l, F jS Y g:i:sa');
        ?>.
+       <h4><p align="center">Original Design by: J. Gavin Ray & Johnathan Adams</p></h4>
    </body>
 </html>
+       <p align="center"><img src="ABM.jpg"></p> -->
