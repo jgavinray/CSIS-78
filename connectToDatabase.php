eff3537660eae6facfc892dde2bc27d73b97b61a
J. Gavin Ray <jgavinray@gmail.com>
2013-03-16 12:04:08 -0700

    MySQL Query Experiments
    Trying to force the query submitted to show the entire populated fields.

--- connectToDatabase.php
+++ connectToDatabase.php
@@ -8,6 +8,8 @@
 //  The database parameter's were implemented by J. Gavin Ray.
 
 //$mysqli = new mysqli("localhost", "root", "", "product");
+
+//$newLink = mysqli_connect("localhost", "root", "", "product");    // New object to acces the product database
 $link = mysqli_connect("localhost", "root", "", "test");
 
 /* check connection */
@@ -29,6 +31,8 @@ else
 /* Select queries return a resultset */
 if ($result = mysqli_query($link, "SELECT * FROM Test LIMIT 10")) {
     printf("<br>Select returned %d rows.\n", mysqli_num_rows($result));
+    printf("<br>Select returned %d fields.\n", mysqli_num_fields($result));
+ //   printf("<br>%s", $result);    // Tried to show the results as a string, its not happy
 
     /* free result set */
     mysqli_free_result($result);
@@ -38,7 +42,7 @@ else
     
 
 /* If we have to retrieve large amount of data we use MYSQLI_USE_RESULT */
-if ($result = mysqli_query($link, "SELECT * FROM Test", MYSQLI_USE_RESULT)) {
+if ($result = mysqli_query($link, "SELECT ID FROM Test", MYSQLI_USE_RESULT)) {
 
     /* Note, that we can't execute any functions which interact with the
        server until result set was closed. All calls will return an
